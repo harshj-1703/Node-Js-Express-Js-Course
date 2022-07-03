8805c1d3c1224b4b6837838946e4771bd8f4e8d8
harshj-1703 <harshj6680@gmail.com>
2022-07-03 17:43:19 +0530

    Warmup-1 sleepin done

---
+++ 1_JavaScript/Assignments/Warmup-1/sleepIn.js
@@ -0,0 +1,25 @@
+/*
+The parameter weekday is true if it is a weekday, 
+and the parameter vacation is true if we are on vacation. 
+We sleep in if it is not a weekday or we're on vacation. 
+Return true if we sleep in.
+*/
+
+function sleepIn(weekday, vacation){
+  if(vacation == true)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+let a = sleepIn(true, true);
+let b = sleepIn(true, false);
+let c = sleepIn(false, false);
+
+console.log(a);
+console.log(b);
+console.log(c);
