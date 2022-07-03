ce194ef56d3196b5e69024d522405db192fc3728
harshj-1703 <harshj6680@gmail.com>
2022-07-03 11:21:30 +0530

    javascript basics

---
+++ 1_JavaScript/02_Basics.js
@@ -0,0 +1,40 @@
+let x,y,z; //variable declaration
+var p;
+
+const q=20;
+
+let xyz = 'xyz';
+xyz = 'pqr';
+
+x=5;
+y=6;
+z=x+y;
+
+console.log(z);
+console.log(xyz);
+
+//difference between == and ===
+let a=3;
+let b='3';
+
+console.log('\na==b',a==b);     //compare just values
+console.log('\na===b',a===b);   //compare values and type
+
+
+//Arithmetic operations
+let s=5;
+let t=4;
+
+console.log('\ns+t : ',s+t);    //addition
+console.log('\ns-t : ',s-t);    //subtraction
+console.log('\ns*t : ',s*t);    //multiplication
+console.log('\ns**t : ',s**t);  //expo
+console.log('\ns/t : ',s/t);    //division
+console.log('\ns%t : ',s%t);    //modulo
+console.log('\ns++ : ',s++);    //increment
+console.log('\ns-- : ',s--);    //decrement
+
+
+//Array fundamentals
+let arr = [1,2,3,4,'1'];
+console.log(arr);
