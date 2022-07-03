b7b308fece57802ff9629c2d936ff2099c54660d
harshj-1703 <harshj6680@gmail.com>
2022-07-03 12:33:09 +0530

    JavaScript Basics

--- 1_JavaScript/02_Basics.js
+++ 1_JavaScript/02_Basics.js
@@ -36,9 +36,12 @@ console.log('\ns-- : ',s--);    //decrement
 
 
 //Array fundamentals
-let arr = [1,2,3,4,'1'];
+let arr = [1,2,3,4,'1',5,6];
 console.log(arr);
-console.log(arr[0]==arr[4]); //compare arrray
+console.log(arr[0]==arr[4]);    //compare arrray  //false
+console.log(arr.sort());        //[1,'1',2,3,4,5,6]
+console.log(arr.length);        //7
+console.log(arr.reverse());     //[6,5,4,3,2,'1',1]
 
 //Object
 let o = {
@@ -62,3 +65,40 @@ console.log(str1.substring(-2,4)); //0,1,2,3
 //substr(startIndex, count)
 console.log(str1.substr(6,2));  //6,7
 console.log(str1.substr(0));    //All
+
+//Functions
+let answer = mul(2,3);
+function mul(a,b){
+    //If-else condition
+    if(a>b)
+    {
+        console.log("A is greater then B");
+    }else if(b>a){
+        console.log("B is greater then A");
+    }else{
+        console.log("B equal to A");
+    }
+
+    //Switch Case
+    switch(a){
+        case 1:
+            console.log('A value is : '+1);
+            break;
+        case 2:
+            console.log('A value is : '+2);
+            break;
+        default:
+            console.log('A value is : '+a);
+            break;
+    }
+
+    // return a*b
+    console.log(a*b);    
+}
+// console.log(answer);
+
+const result = arr.filter((item) => {
+    return item >= 4;
+});
+
+console.log("\nFiltered value of array : "+result);
