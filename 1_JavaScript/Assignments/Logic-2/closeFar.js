19f71493686be29a8875cd027bdde95d4b28cd5b
harshj-1703 <harshj6680@gmail.com>
2022-07-06 22:39:57 +0530

    Logic-2 => closeFar done

---
+++ 1_JavaScript/Assignments/Logic-2/closeFar.js
@@ -0,0 +1,18 @@
+/*
+Given three ints, a b c, return true if one of b or c 
+is "close" (differing from a by at most 1), while the 
+other is "far", differing from both other values by 2 or more. 
+Note: Math.abs(num) computes the absolute value of a number.
+*/
+
+function closeFar(a, b, c){
+    
+}
+
+let a = closeFar(1, 2, 10);
+let b = closeFar(1, 2, 3);
+let c = closeFar(4, 1, 3);
+
+console.log(a);
+console.log(b);
+console.log(c);
