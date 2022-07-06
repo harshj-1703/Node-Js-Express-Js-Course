74b017ada69f56c6298b95b934e63a736a9f0f8e
harshj-1703 <harshj6680@gmail.com>
2022-07-06 22:40:35 +0530

    Logic-2 => closeFar done

--- 1_JavaScript/Assignments/Logic-2/closeFar.js
+++ 1_JavaScript/Assignments/Logic-2/closeFar.js
@@ -6,7 +6,18 @@ Note: Math.abs(num) computes the absolute value of a number.
 */
 
 function closeFar(a, b, c){
-    
+    if (Math.abs(a - b) <= 1 && Math.abs(a - c) >= 2 && Math.abs(b - c) >= 2)
+    {
+        return true;
+    }
+    else if (Math.abs(a - c) <= 1 && Math.abs(a - b) >= 2 && Math.abs(b - c) >= 2)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 let a = closeFar(1, 2, 10);
