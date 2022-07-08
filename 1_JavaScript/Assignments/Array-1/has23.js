31017801af80b383df8eb6e586517381bde11a81
harshj-1703 <harshj6680@gmail.com>
2022-07-08 11:22:34 +0530

    Array-1 => has23 done

--- 1_JavaScript/Assignments/Array-1/has23.js
+++ 1_JavaScript/Assignments/Array-1/has23.js
@@ -1,10 +1,9 @@
 //Given an int array length 2, return true if it contains a 2 or a 3.
 
 function has23(nums){
-    if(nums[0] == 2 || nums[0] == 3
-       || nums[1] == 2 || nums[1] == 3)
+    if(nums.includes(2) || nums.includes(3))
     {
-    return true;
+        return true;
     }
     else
     {
