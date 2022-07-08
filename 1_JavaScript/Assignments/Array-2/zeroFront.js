eef7a9cca17bb3167bb601c909837b823c524cfa
harshj-1703 <harshj6680@gmail.com>
2022-07-08 22:33:04 +0530

    Array-2 => zeroFront done

---
+++ 1_JavaScript/Assignments/Array-2/zeroFront.js
@@ -0,0 +1,37 @@
+/*
+Return an array that contains the exact same numbers as the 
+given array, but rearranged so that all the zeros are grouped 
+at the start of the array. The order of the non-zero numbers does 
+not matter. So {1, 0, 0, 1} becomes {0 ,0, 1, 1}. You may modify 
+and return the given array or make a new array.
+*/
+
+function zeroFront(nums){
+    if(nums.length == 0)
+    {
+        return nums;
+    }
+          
+    i = 0;          
+    while(nums[i] == 0)
+        i++;
+                        
+    for(j = i + 1; j < nums.length; j++) {
+        if(nums[j] == 0) {
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+        }
+    }
+                                                              
+    return nums;
+}
+
+let a = zeroFront([1, 0, 0, 1]); //→ 0,0,1,1
+let b = zeroFront([0, 1, 1, 0, 1]); //→ 0,0,1,1,1
+let c = zeroFront([1, 0]); //→ 0,1
+
+console.log(a);
+console.log(b);
+console.log(c);
