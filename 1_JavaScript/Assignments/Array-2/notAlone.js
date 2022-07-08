2dde9ee56b4f078ef87429343b136f7de272c49c
harshj-1703 <harshj6680@gmail.com>
2022-07-08 22:26:55 +0530

    Array-2 => notAlone done

---
+++ 1_JavaScript/Assignments/Array-2/notAlone.js
@@ -0,0 +1,36 @@
+/*
+We'll say that an element in an array is "alone" if there are 
+values before and after it, and those values are different from 
+it. Return a version of the given array where every instance of 
+the given value which is alone is replaced by whichever value to 
+its left or right is larger.
+*/
+
+function notAlone(nums, val){
+    for (i= 1;i<nums.length -1;i++)
+	{
+	  if (nums[i] == val)
+      {
+        if (nums[i-1] != nums[i] && nums[i+1] != nums[i])
+        {
+            if (nums[i+1] > nums[i-1]) 
+            {
+                nums[i] = nums[i+1];
+            }
+            else if (nums[i+1] < nums[i-1])
+            {
+                nums[i] = nums[i-1];
+            }
+        }
+      }
+	}
+	return nums;
+}
+
+let a = notAlone([1, 2, 3], 2); //→ 1,3,3
+let b = notAlone([1, 2, 3, 2, 5, 2], 2); //→ 1,3,3,5,5,2
+let c = notAlone([3, 4], 3); //→ 3,4
+
+console.log(a);
+console.log(b);
+console.log(c);
