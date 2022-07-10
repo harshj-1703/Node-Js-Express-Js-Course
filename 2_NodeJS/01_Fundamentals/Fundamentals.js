c0dd6e3543045f0784a1c2c63ae8980e5b45a516
harshj-1703 <harshj6680@gmail.com>
2022-07-10 10:31:10 +0530

    Fundamentals

---
+++ 2_NodeJS/01_Fundamentals/Fundamentals.js
@@ -0,0 +1,3 @@
+//Export variables------------------------------------------------------------
+const exportedVariables = require('./export-variables');
+console.log('Exported function from other file : ',exportedVariables.sumXY());
