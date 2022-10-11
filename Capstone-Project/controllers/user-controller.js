8504d5d08bc8440b9db15a2732122bdfafea2e01
harshj-1703 <harshj6680@gmail.com>
2022-10-11 09:18:51 +0530

    user collection insert in monodb

--- Capstone-Project/controllers/user-controller.js
+++ Capstone-Project/controllers/user-controller.js
@@ -32,7 +32,7 @@ class UserController{
                 email: req.body.email,
                 password: req.body.password,
                 mobile_number: req.body.mobile_number,
-                age: req.body.age,
+                /* age: req.body.age, */
             }
             console.log("userData", userData);
             let queryResponse = await this.userservice.createUser(userData);
