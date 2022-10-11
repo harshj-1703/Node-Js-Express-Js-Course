acc74c1a40bf6740b31aa9c1b4f6dafda1038884
harshj-1703 <harshj6680@gmail.com>
2022-10-11 09:16:02 +0530

    users collection inserting

---
+++ Capstone-Project/models/users.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+var userSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required: true,
+    },
+    email : {
+        type: String,
+        required: true,
+    },
+    password : {
+        type: String,
+        required: true,
+    },
+    mobile_number : {
+        type: String,
+        minLength: 10,
+        maxLength: 10,
+        required: true,
+    },
+})
+
+module.exports = mongoose.model("users",userSchema)
---
+++ Capstone-Project/routes/index.js
@@ -0,0 +1,36 @@
+class IndexRoute{ 
+    constructor(){ 
+        this.router = require("express").Router()
+        this.userController = require('../controllers/user-controller');
+        this.setRoutes()
+    } 
+ 
+    setRoutes(){ 
+        this.router.get('/', (req, res) => { 
+            res.status(200).send({ 
+                status: true, 
+                message: "I got the response",
+            }) 
+        })
+        
+        // this.router.post('/verify-user',this.userController.verifyUser.bind(this.userController))   //done
+
+        this.router.post('/create-user',this.userController.createUser.bind(this.userController))   //done
+
+        // this.router.get('/find-user',this.userController.findUser.bind(this.userController))  //done
+
+        // this.router.get('/all-users',this.userController.getuser.bind(this.userController)) //no-need code
+
+        // this.router.delete('/delete-users',this.userController.deleteUser.bind(this.userController))    //done
+
+        // this.router.patch('/update-user',this.userController.updateUser.bind(this.userController))  //done
+
+        //Api to insert-data in user data collection
+        // this.router.post('/insert-user-note',this.userController.insertUserData.bind(this.userController))  //done
+
+        // this.router.get('/collect-user-note/:id',this.userController.collectUserData.bind(this.userController))     //done
+    } 
+} 
+
+const router = new IndexRoute();
+module.exports = router.router;
