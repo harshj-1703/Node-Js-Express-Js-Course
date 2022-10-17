5ba3147176ed30f6106e81ea6396aebfaaa0d3f6
harshj-1703 <harshj6680@gmail.com>
2022-10-17 13:04:13 +0530

    Capstone project

--- Capstone-Project/routes/index.js
+++ Capstone-Project/routes/index.js
@@ -14,23 +14,23 @@ class IndexRoute{
             }) 
         })
         
-        this.router.post('/verify-user',this.userController.verifyUser.bind(this.userController))
+        this.router.post('/verify-user',this.userController.verifyUser.bind(this.userController))   //LOG IN
 
-        this.router.post('/create-user',this.userController.createUser.bind(this.userController))
+        this.router.post('/create-user',this.userController.createUser.bind(this.userController))   //SIGN IN
 
-        this.router.post('/create-job',this.jobController.createJobs.bind(this.jobController))
+        this.router.post('/create-job',this.jobController.createJobs.bind(this.jobController))      //CREATE JOB
 
-        this.router.patch('/update-job',this.jobController.updateJobs.bind(this.jobController))
+        this.router.patch('/update-job',this.jobController.updateJobs.bind(this.jobController))     //UPDATE JOB
 
-        this.router.delete('/delete-job',this.jobController.deleteJobs.bind(this.jobController))
+        this.router.delete('/delete-job',this.jobController.deleteJobs.bind(this.jobController))    //DELETE JOB
 
-        this.router.get('/get-detail-job',this.jobController.findJob.bind(this.jobController))
+        this.router.get('/get-detail-job',this.jobController.findJob.bind(this.jobController))      //GET JOB
 
-        this.router.get('/all-jobs',this.jobController.getJobs.bind(this.jobController))
+        this.router.get('/all-jobs',this.jobController.getJobs.bind(this.jobController))            //GET ALL JOBS
 
-        this.router.get('/search-jobs',this.jobController.searchJobs.bind(this.jobController))
+        this.router.get('/search-jobs',this.jobController.searchJobs.bind(this.jobController))      //SERACH JOBS
 
-        this.router.get('/sort-job-salary',this.jobController.sortJobs.bind(this.jobController))
+        this.router.get('/sort-job-salary',this.jobController.sortJobs.bind(this.jobController))    //SORT JOBS
 
         // this.router.delete('/delete-users',this.userController.deleteUser.bind(this.userController))    //done
 
